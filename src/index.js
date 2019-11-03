dcfd4cd1b21a519215f464d04c897431d8c84a45
Linesh <linesh.simpcity@gmail.com>
2019-11-03 22:08:39 +0800

    first quick implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-export const babelWorksWithExports = () => true
+export class Length {
+  value
+  unit
 
-export class PrivateClass {
-  #number
-
-  constructor(number) {
-    this.#number = number
+  constructor(val, uint) {
+    this.value = val
+    this.unit = uint
   }
 
-  getDouble() {
-    return this.#number * 2
+  getVal() {
+    return this.value
+  }
+
+  getUint() {
+    return this.unit
+  }
+
+  parseTo(u) {
+    let len = this
+    if (this.unit === 'yard') {
+      if (u === 'f') {
+        len = new Length(this.value * 3, u)
+      } else if (u === 'inch') {
+        len = new Length(this.value * 36, u)
+      }
+    }
+
+    if (this.unit === 'inch') {
+      if (u === 'yard') {
+        len = new Length(this.value / 36, u)
+      } else if (u === 'f') {
+        len = new Length(this.value / 12, u)
+      }
+    }
+
+    if (this.unit === 'f') {
+      if (u === 'yard') {
+        len = new Length(this.value / 3, u)
+      } else if (u === 'inch') {
+        len = new Length(this.value * 12, u)
+      }
+    }
+
+    return len
   }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,66 @@
-import { babelWorksWithExports, PrivateClass } from './index'
+import { Length } from './index'
 
-it('should jest works', () => {
-  expect(babelWorksWithExports()).toEqual(true)
-})
+describe('Length', () => {
+  it("should 1 'foot' equals 1 'foot'", () => {
+    const length = new Length(1, 'f')
 
-it('should support private class methods', () => {
-  const result = new PrivateClass(2).getDouble()
+    expect(length.getVal()).toEqual(1)
+    expect(length.getUint()).toEqual('f')
+  })
 
-  expect(result).toEqual(4)
+  it("should 1 'foot' equals 12 inches", () => {
+    const result = new Length(1, 'f').parseTo('inch')
+
+    expect(result.getVal()).toEqual(12)
+    expect(result.getUint()).toEqual('inch')
+  })
+
+  it("should 3 feet equals 1 'yard'", () => {
+    const result = new Length(3, 'f').parseTo('yard')
+
+    expect(result.getVal()).toEqual(1)
+    expect(result.getUint()).toEqual('yard')
+  })
+
+  it("should 1 'yard' equals 3 feet", () => {
+    const result = new Length(1, 'yard').parseTo('f')
+
+    expect(result.getVal()).toEqual(3)
+    expect(result.getUint()).toEqual('f')
+  })
+
+  it("should 1 'yard' equals 36 inches", () => {
+    const result = new Length(1, 'yard').parseTo('inch')
+
+    expect(result.getVal()).toEqual(36)
+    expect(result.getUint()).toEqual('inch')
+  })
+
+  it("should 1 'yard' equals 1 'yard'", () => {
+    const result = new Length(1, 'yard').parseTo('yard')
+
+    expect(result.getVal()).toEqual(1)
+    expect(result.getUint()).toEqual('yard')
+  })
+
+  it("should 12 inches equals 1 'foot'", () => {
+    const result = new Length(12, 'inch').parseTo('f')
+
+    expect(result.getVal()).toEqual(1)
+    expect(result.getUint()).toEqual('f')
+  })
+
+  it("should 36 inches equals 1 'yard'", () => {
+    const result = new Length(36, 'inch').parseTo('yard')
+
+    expect(result.getVal()).toEqual(1)
+    expect(result.getUint()).toEqual('yard')
+  })
+
+  it("should 1 inch equals 1 'inch'", () => {
+    const result = new Length(1, 'inch').parseTo('inch')
+
+    expect(result.getVal()).toEqual(1)
+    expect(result.getUint()).toEqual('inch')
+  })
 })
