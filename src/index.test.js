a9f2bcf4825073cb109c144d170c7fdb63b95aa0
Linesh <linesh.simpcity@gmail.com>
2020-01-12 22:26:55 +0800

    refactor: continue the replacement for all Length.FOOT

--- src/index.test.js
+++ src/index.test.js
@@ -5,28 +5,28 @@ describe('Length', () => {
     const length = new Length(1, Length.FOOT)
 
     expect(length.getValue()).toEqual(1)
-    expect(length.getUnit()).toEqual('f')
+    expect(length.getUnit()).toEqual(Length.FOOT)
   })
 
   it("should 1 'foot' equals 12 inches", () => {
-    const result = new Length(1, 'f').parseTo('inch')
+    const result = new Length(1, Length.FOOT).parseTo('inch')
 
     expect(result.getValue()).toEqual(12)
     expect(result.getUnit()).toEqual('inch')
   })
 
   it("should 3 feet equals 1 'yard'", () => {
-    const result = new Length(3, 'f').parseTo('yard')
+    const result = new Length(3, Length.FOOT).parseTo('yard')
 
     expect(result.getValue()).toEqual(1)
     expect(result.getUnit()).toEqual('yard')
   })
 
   it("should 1 'yard' equals 3 feet", () => {
-    const result = new Length(1, 'yard').parseTo('f')
+    const result = new Length(1, 'yard').parseTo(Length.FOOT)
 
     expect(result.getValue()).toEqual(3)
-    expect(result.getUnit()).toEqual('f')
+    expect(result.getUnit()).toEqual(Length.FOOT)
   })
 
   it("should 1 'yard' equals 36 inches", () => {
@@ -44,10 +44,10 @@ describe('Length', () => {
   })
 
   it("should 12 inches equals 1 'foot'", () => {
-    const result = new Length(12, 'inch').parseTo('f')
+    const result = new Length(12, 'inch').parseTo(Length.FOOT)
 
     expect(result.getValue()).toEqual(1)
-    expect(result.getUnit()).toEqual('f')
+    expect(result.getUnit()).toEqual(Length.FOOT)
   })
 
   it("should 36 inches equals 1 'yard'", () => {
