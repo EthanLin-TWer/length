f6c91af0c201c4aaedeff9ce00efc7df4526e25d
Linesh <linesh.simpcity@gmail.com>
2020-01-12 22:44:47 +0800

    refactor: remove unused result variable

--- src/index.js
+++ src/index.js
@@ -19,34 +19,27 @@ export class Length {
 
   parseTo(unit) {
     let value = this.value
-    let result = new Length(value, unit)
     if (this.unit === Unit.YARD) {
       if (unit === Unit.FOOT) {
         value = this.value * 3
-        result = new Length(value, unit)
       } else if (unit === Unit.INCH) {
         value = this.value * 36
-        result = new Length(value, unit)
       }
     }
 
     if (this.unit === Unit.INCH) {
       if (unit === Unit.YARD) {
         value = this.value / 36
-        result = new Length(value, unit)
       } else if (unit === Unit.FOOT) {
         value = this.value / 12
-        result = new Length(value, unit)
       }
     }
 
     if (this.unit === Unit.FOOT) {
       if (unit === Unit.YARD) {
         value = this.value / 3
-        result = new Length(value, unit)
       } else if (unit === Unit.INCH) {
         value = this.value * 12
-        result = new Length(value, unit)
       }
     }
 
